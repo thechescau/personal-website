d57f656f9e9d523fb16ec9a1be8ead0fe775f4c3
nicolefranchesca <nicolefranchescausi@gmail.com>
2025-09-15 16:27:56 +0800

    Refactor Navbar

--- personal-website/src/components/Navbar.tsx
+++ personal-website/src/components/Navbar.tsx
@@ -6,7 +6,6 @@ type NavItem = {
 }
 
 const navItems: NavItem[] = [
-    { label: "Home", href: "#" },
     { label: "About", href: "#about" },
     { label: "Skills", href: "#skills" },
     { label: "Projects", href: "#projects"},
@@ -26,7 +25,7 @@ function Navbar()  {
                         setActiveSection(entry.target.id)
                     }
                 })
-            }, {threshold: 0.6}
+            }, {threshold: 0.3}
         )
 
         sections.forEach((section) => observer.observe(section))
@@ -38,21 +37,38 @@ function Navbar()  {
 
 
     return (
-        <nav className="bg-dawn-pink px-6 py-4 justify-between items-center flex sticky top-0 z-50">
-
-            {/* Logo */}
-            <div className="text-xl font-bold pl-5 text-dune"> thechescau </div>
+        <nav className="bg-kombu-green w-[90%] mx-auto mt-4 px-6 py-4 justify-between items-center flex sticky top-0 z-50">
 
             {/* Links */}
-            <ul className="flex space-x-8 pr-5">
-                {navItems.map((item) => (
+            <ul className="flex space-x-20 pl-10 text-white">
+                {navItems.slice(0, 2).map((item) => (
                     <li key={item.label}>
                         <a
                             href={item.href}
                             className={`transition-colors ${
                                 activeSection === item.href.substring(1) 
-                                ? "border-b-2 border-dune text-dune"
-                                : "hover:text-dune"
+                                ? "border-b-2 border-white text-white"
+                                : "hover:text-white"
+                            }`}
+                        >
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+
+            {/* Center Logo */}
+            <div className="text-xl font-bold text-white text-center"> thechescau </div>
+
+            <ul className="flex space-x-20 pr-10 text-white">
+                {navItems.slice(2).map((item) => (
+                    <li key={item.label}>
+                        <a
+                            href={item.href}
+                            className={`transition-colors ${
+                                activeSection === item.href.substring(1) 
+                                ? "border-b-2 border-white text-white"
+                                : "hover:text-white"
                             }`}
                         >
                             {item.label}
